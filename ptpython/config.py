7370ec43e0a9155aadc6299f24c5f1dce825562f
Eduardo Miyashiro Asato <ed.miya.a@gmail.com>
2023-02-27 00:35:56 +0100

    Changes to ptpython config

--- ptpython/config.py
+++ ptpython/config.py
@@ -2,11 +2,8 @@ from ptpython.layout import CompletionVisualisation
 
 
 def configure(repl):
-    repl.vi_mode = True
     repl.insert_blank_line_after_output = False
-    repl.completion_visualisation = CompletionVisualisation.POP_UP
-    repl.show_docstring = True
-    repl.enable_fuzzy_completion = True
+    repl.completion_visualisation = CompletionVisualisation.NONE
     repl.use_code_colorscheme('vim')
-    repl.confirm_exit = False
+    repl.show_status_bar = False
 
